39e47f33409ba70bd89e0fd68a6110df9269ef28
72OTA <f.andradevalenzuela@gmail.com>
2022-10-17 22:57:47 -0300

    added sound effects

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
